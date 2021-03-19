6c0bbb9fc4b261f46b6a9a7af2917656b22b83ee
ing928 <78721578+ing928@users.noreply.github.com>
2021-03-19 14:01:45 +0900

    bg-Img-js

---
+++ js/bg.js
@@ -0,0 +1,23 @@
+const body = document.querySelector("body");
+
+const IMG_NUMBER = 2;
+
+
+function paintBg(num){
+    const image = new Image();
+    image.src = `img/${num+1}.png`;
+    image.classList.add("bgImg");
+    body.prepend(image);
+}
+
+function getRandom(){
+    const number = Math.floor(Math.random()*IMG_NUMBER);
+    return number;
+}
+
+function init(){
+    const imageNum = getRandom();
+    paintBg(imageNum);
+}
+
+init();
